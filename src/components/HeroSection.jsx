d91de1914270372a7a153f08288c27d1b4c36ea2
SniperGod <103439202+AkshatMahadeva@users.noreply.github.com>
2023-09-24 12:34:42 +0530

    commitable

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,14 +4,15 @@ import { FaSearch } from 'react-icons/fa';
 const HeroSection = () => {
     return (
         <section className="hero relative min-h-screen flex flex-col items-center justify-center">
-            <div className='container z-10 px-5 mx-auto text-center py-16 flex flex-col gap-10'>
-                <h1 className="text-4xl md:text-6xl font-bold leading-tight text-white">
-                    Take Your <span className="text-red-500">Business</span> Next Level
-                </h1>
-                <p className="text-lg md:text-xl mb-8 text-gray-200">
-                    Explore the world with stunning views and unforgettable experiences.
-                </p>
-                <form className="mx-auto flex flex-col md:flex-row gap-5 justify-center  items-start md:items-center p-5 bg-white text-gray-700 rounded-lg w-fit border">
+            <div className='container z-10 px-5 mx-auto text-center py-10 flex flex-col gap-10'>
+                <div className='max-w-[700px] w-full mx-auto text-center flex flex-col items-center gap-5 py-5'>
+                    <h1 className='text-4xl md:text-6xl font-semibold text-white'>
+                        Take Your <span className=' text-red-500'>Business</span> To Next Level
+                    </h1>
+                    <hr className=' border-4 border-red-400 w-10' />
+                    <p className=' text-gray-200'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit enim quisquam minus accusamus eaque natus! Laboriosam sequi fugit, dolore eos.</p>
+                </div>
+                <form className="mx-auto flex flex-col md:flex-row gap-5 justify-center  items-start md:items-center py-5 px-5 md:px-10 bg-white text-gray-700 rounded-md md:rounded-full w-fit border">
                     <div className=' text-left md:border-r w-full'>
                         <label htmlFor="area" className='p-2'>
                             Area
@@ -58,7 +59,7 @@ const HeroSection = () => {
                     </button>
                 </form>
             </div>
-            <div className=' absolute inset-0 bg-black bg-opacity-80 z-0'></div>
+            <div className=' absolute inset-0 bg-black bg-opacity-[0.88] z-0'></div>
         </section >
     );
 };
